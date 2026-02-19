fd02d88ac9cc9681ece303b2bad61956283f5e07
yaseen8603 <154041599+yaseen8603@users.noreply.github.com>
2026-02-19 22:58:53 +0530

    Add files via upload

---
+++ StudentGradeEvaluator.js
@@ -0,0 +1,21 @@
+function studentGrade(){
+    let marks=86;
+    if(marks>=75){
+        console.log('A');
+    }
+    if(marks>=60){
+        console.log('B');
+    }
+    if(marks>=40){
+        console.log('C');
+    }
+    else(marks<40){
+        console.log('FAIL');
+    }
+   
+    
+
+
+    
+
+}
---
+++ signal.js
@@ -0,0 +1,19 @@
+
+let signal = "yellow";   
+
+switch (signal) {
+    case "red":
+        console.log("Stop");
+        break;
+
+    case "yellow":
+        console.log("Get Ready");
+        break;
+
+    case "green":
+        console.log("Go");
+        break;
+
+    default:
+        console.log("Invalid signal color");
+}
